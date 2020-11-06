c5354af27505e2f6a7fe4d4669c84400500f580a
Matthew Garrett <mjg@redhat.com>
2020-11-06 08:36:36 +0000

    Add support for Linux EFI stub loading on aarch64.
    Add support for Linux EFI stub loading on aarch64.
    
    v1:
    Make efi handoff the default loader for arm64 platform.
    
    v2:
    The efi shim_lock verifier has been moved to grub core so local
    shim_lock protocol is no longer needed here for aarch64 efi to verify
    the loaded kernel image. From now on the framework will take care the
    verificaion, consolidating the integration of various security verifiers
    like secure boot, gpg and tpm.
    
    v3:
    Drop argv argument in grub_initrd_load.

---
+++ include/grub/arm64/linux.h
@@ -0,0 +1,36 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2013  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GRUB_ARM64_LINUX_HEADER
+#define GRUB_ARM64_LINUX_HEADER 1
+
+#include <grub/types.h>
+#include <grub/efi/pe32.h>
+
+struct grub_arm64_linux_pe_header
+{
+  grub_uint32_t magic;
+  struct grub_pe32_coff_header coff;
+  struct grub_pe64_optional_header opt;
+};
+
+#if defined(__aarch64__)
+# define grub_armxx_linux_pe_header grub_arm64_linux_pe_header
+#endif
+
+#endif /* ! GRUB_ARM64_LINUX_HEADER */
