b8fbb4eaa3a18274b8423ce829848107186046b5
Hernan Gatta <hegatta@linux.microsoft.com>
2022-02-01 05:02:54 -0800

    tpm2: Add TPM Software Stack (TSS)
    A Trusted Platform Module (TPM) Software Stack (TSS) provides logic to
    compose, submit, and parse TPM commands and responses.
    
    A limited number of TPM commands may be accessed via the EFI TCG2
    protocol. This protocol exposes functionality that is primarily geared
    toward TPM usage within the context of Secure Boot. For all other TPM
    commands, however, such as sealing and unsealing, this protocol does not
    provide any help, with the exception of passthrough command submission.
    
    The SubmitCommand method allows a caller to send raw commands to the
    system's TPM and to receive the corresponding response. These
    command/response pairs are formatted using the TPM wire protocol. To
    construct commands in this way, and to parse the TPM's response, it is
    necessary to, first, possess knowledge of the various TPM structures, and,
    second, of the TPM wire protocol itself.
    
    As such, this patch includes a set of header files that define the
    necessary TPM structures and TSS functions, implementations of various
    TPM2_* functions (inventoried below), and logic to write and read command
    and response buffers, respectively, using the TPM wire protocol.
    
    Functions: TPM2_Create, TPM2_CreatePrimary, TPM2_EvictControl,
    TPM2_FlushContext, TPM2_Load, TPM2_PCR_Read, TPM2_PolicyGetDigest,
    TPM2_PolicyPCR, TPM2_ReadPublic, TPM2_StartAuthSession, TPM2_Unseal.
    
    Signed-off-by: Hernan Gatta <hegatta@linux.microsoft.com>
    Signed-off-by: Gary Lin <glin@suse.com>

---
+++ include/grub/tpm2/internal/functions.h
@@ -0,0 +1,117 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2022 Microsoft Corporation
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GRUB_TPM2_INTERNAL_FUNCTIONS_HEADER
+#define GRUB_TPM2_INTERNAL_FUNCTIONS_HEADER 1
+
+#include <grub/tpm2/internal/structs.h>
+
+TPM_RC
+TPM2_CreatePrimary (const TPMI_RH_HIERARCHY primaryHandle,
+		    const TPMS_AUTH_COMMAND *authCommand,
+		    const TPM2B_SENSITIVE_CREATE *inSensitive,
+		    const TPM2B_PUBLIC *inPublic,
+		    const TPM2B_DATA *outsideInfo,
+		    const TPML_PCR_SELECTION *creationPCR,
+		    TPM_HANDLE *objectHandle,
+		    TPM2B_PUBLIC *outPublic,
+		    TPM2B_CREATION_DATA *creationData,
+		    TPM2B_DIGEST *creationHash,
+		    TPMT_TK_CREATION *creationTicket,
+		    TPM2B_NAME *name,
+		    TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_StartAuthSession (const TPMI_DH_OBJECT tpmKey,
+		       const TPMI_DH_ENTITY bind,
+		       const TPMS_AUTH_COMMAND *authCommand,
+		       const TPM2B_NONCE *nonceCaller,
+		       const TPM2B_ENCRYPTED_SECRET *encryptedSalt,
+		       const TPM_SE sessionType,
+		       const TPMT_SYM_DEF *symmetric,
+		       const TPMI_ALG_HASH authHash,
+		       TPMI_SH_AUTH_SESSION *sessionHandle,
+		       TPM2B_NONCE *nonceTpm,
+		       TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_PolicyPCR (const TPMI_SH_POLICY policySession,
+		const TPMS_AUTH_COMMAND *authCommand,
+		const TPM2B_DIGEST *pcrDigest,
+		const TPML_PCR_SELECTION *pcrs,
+		TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_ReadPublic (const TPMI_DH_OBJECT objectHandle,
+		 const TPMS_AUTH_COMMAND* authCommand,
+		 TPM2B_PUBLIC *outPublic);
+
+TPM_RC
+TPM2_Load (const TPMI_DH_OBJECT parent_handle,
+	   const TPMS_AUTH_COMMAND *authCommand,
+	   const TPM2B_PRIVATE *inPrivate,
+	   const TPM2B_PUBLIC *inPublic,
+	   TPM_HANDLE *objectHandle,
+	   TPM2B_NAME *name,
+	   TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_Unseal (const TPMI_DH_OBJECT item_handle,
+	     const TPMS_AUTH_COMMAND *authCommand,
+	     TPM2B_SENSITIVE_DATA *outData,
+	     TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_FlushContext (const TPMI_DH_CONTEXT handle);
+
+TPM_RC
+TPM2_PCR_Read (const TPMS_AUTH_COMMAND *authCommand,
+	       const TPML_PCR_SELECTION *pcrSelectionIn,
+	       grub_uint32_t *pcrUpdateCounter,
+	       TPML_PCR_SELECTION *pcrSelectionOut,
+	       TPML_DIGEST *pcrValues,
+	       TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_PolicyGetDigest (const TPMI_SH_POLICY policySession,
+		      const TPMS_AUTH_COMMAND *authCommand,
+		      TPM2B_DIGEST *policyDigest,
+		      TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_Create (const TPMI_DH_OBJECT parentHandle,
+	     const TPMS_AUTH_COMMAND *authCommand,
+	     const TPM2B_SENSITIVE_CREATE *inSensitive,
+	     const TPM2B_PUBLIC *inPublic,
+	     const TPM2B_DATA *outsideInfo,
+	     const TPML_PCR_SELECTION *creationPCR,
+	     TPM2B_PRIVATE *outPrivate,
+	     TPM2B_PUBLIC *outPublic,
+	     TPM2B_CREATION_DATA *creationData,
+	     TPM2B_DIGEST *creationHash,
+	     TPMT_TK_CREATION *creationTicket,
+	     TPMS_AUTH_RESPONSE *authResponse);
+
+TPM_RC
+TPM2_EvictControl (const TPMI_RH_PROVISION auth,
+		   const TPMI_DH_OBJECT objectHandle,
+		   const TPMS_AUTH_COMMAND *authCommand,
+		   const TPMI_DH_PERSISTENT persistentHandle,
+		   TPMS_AUTH_RESPONSE *authResponse);
+
+#endif /* ! GRUB_TPM2_INTERNAL_FUNCTIONS_HEADER */
