74675838e7f872b6270a9f094bed01e19d92e027
Diego Domingos <diegodo@br.ibm.com>
2022-03-15 15:59:41 -0400

    ieee1275/ofpath: enable NVMeoF logical device translation
    This patch add code to enable the translation of logical devices to the of NVMeoFC paths.

--- include/grub/util/ofpath.h
+++ include/grub/util/ofpath.h
@@ -3,4 +3,33 @@
 
 char *grub_util_devname_to_ofpath (const char *devname);
 
+struct ofpath_files_list_node {
+  char* filename;
+  struct ofpath_files_list_node* next;
+};
+
+struct ofpath_files_list_root {
+  int items;
+  struct ofpath_files_list_node* first;
+};
+
+struct ofpath_nvmeof_info {
+  char* host_wwpn;
+  char* target_wwpn;
+  char* nqn;
+  int cntlid;
+  int nsid;
+};
+
+void of_path_get_nvmeof_adapter_info(char* sysfs_path,
+                               struct ofpath_nvmeof_info* nvmeof_info);
+
+unsigned int of_path_get_nvme_nsid(const char* devname);
+
+void add_filename_to_pile(char *filename, struct ofpath_files_list_root* root);
+
+void find_file(char* filename, char* directory, struct ofpath_files_list_root* root, int max_depth, int depth);
+
+char* of_find_fc_host(char* host_wwpn);
+
 #endif /* ! GRUB_OFPATH_MACHINE_UTIL_HEADER */
