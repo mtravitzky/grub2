4cc834e979f54f9e5950abed3cb36b61c7a404cd
mtravitzky <marcelias.travitzky@suse.com>
2024-07-09 14:50:46 +0200

    Experimental groundwork

--- grub-core/net/efi/dns.c
+++ grub-core/net/efi/dns.c
@@ -6,13 +6,24 @@
 #include <grub/net/efi.h>
 #include <grub/charset.h>
 
+static grub_guid_t dns4_protocol_guid = GRUB_EFI_DNS4_PROTOCOL_GUID;
+
 static grub_err_t
 grub_cmd_efi_list_dns (
     struct grub_command *cmd __attribute__ ((unused)),
     int argc  __attribute__ ((unused)),
     char **args __attribute__ ((unused)))
 {
-  grub_printf ("Unimplemented\n");
+  grub_printf ("Experimental\n");
+  grub_efi_handle_t *handles;
+  grub_efi_uintn_t num_handles;
+  //unsigned i, j, k;
+
+  handles = grub_efi_locate_handle (GRUB_EFI_BY_PROTOCOL, &dns4_protocol_guid,
+				    0, &num_handles);
+
+  grub_printf ("Handles found: %d", num_handles);
+
   return GRUB_ERR_NONE;
 }
 
